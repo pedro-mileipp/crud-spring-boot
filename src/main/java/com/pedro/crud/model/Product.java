99576d97c019bcf15542bb6eaa6277970c83a672
pedro-mileipp <poncianopedro70@gmail.com>
2025-08-15 11:56:34 -0300

    adiciona a camada Model para representar os dados da aplicacao

---
+++ src/main/java/com/pedro/crud/model/Product.java
@@ -0,0 +1,54 @@
+package com.pedro.crud.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity(name="product")
+@Table(name="product") 
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private Long price;
+    
+    public Product() {
+    }
+
+    public Product(UUID id, String name, Long price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+}
